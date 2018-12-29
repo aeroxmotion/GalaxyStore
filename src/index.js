d24c239a0b3e0ab9e77c1049e92fee26d76561e6
Camilo Rodríguez <aeroxmotion@hotmail.com>
2018-12-29 14:46:04 -0500

    chore: bump to new plugins API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
+import { GalaxyPlugin } from 'https://cdn.jsdelivr.net/gh/LosMaquios/GalaxyJS/dist/galaxy.esm.js'
+
+import GalaxyStore from './GalaxyStore.js'
+
 /**
- * Export main plugin
+ * Export main class
  */
-export { default as GalaxyStore } from './GalaxyStore.js'
+export { GalaxyStore }
+
+/**
+ * Export plugin
+ */
+export default class GalaxyStorePlugin extends GalaxyPlugin {
+  static init () {
+    this.$store = new GalaxyStore(this.$options)
+  }
+
+  static install (GalaxyElement) {
+    GalaxyElement.prototype.$store = this.$store
+  }
+}
